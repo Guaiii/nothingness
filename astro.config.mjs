305c377c639481249869ec4a0fbc4c8c0b8e7b5e
guai <xindi2570@163.com>
2024-11-06 08:10:41 +0800

    重改首页代码

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,22 @@ export default defineConfig({
         resolve: {
             alias: [{find: '@', replacement: '/src'}]
         }
+    },
+    markdown: {
+        shikiConfig: {
+            // 选择 Shiki 内置的主题（或添加你自己的主题）
+            // https://shiki.style/themes
+            theme: 'solarized-light',
+            // 禁用默认配色
+            // https://shiki.style/guide/dual-themes#without-default-color
+            // (添加于 v4.12.0)
+            defaultColor: false,
+            // 添加自定义语言
+            // 注意：Shiki 内置了无数语言，包括 .astro！
+            // https://shiki.style/languages
+            langs: ['astro', 'js', 'ts', 'jsx', 'tsx'],
+            // 启用自动换行，以防止水平滚动
+            wrap: true,
+        },
     }
 });
